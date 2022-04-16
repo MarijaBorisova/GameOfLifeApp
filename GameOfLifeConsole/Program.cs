853b906dd538a3bbd79bd53e00f311cd445fc3c2
Marija Borisova <marija.borisova@accenture.com>
2022-04-16 19:10:30 +0300

    Bug ir recognized and resolved: no clear action, everytime should be rerun. Resolved. Another symbol input- the error is shown. Random variant also works properly.

--- GameOfLifeConsole/Program.cs
+++ GameOfLifeConsole/Program.cs
@@ -4,15 +4,21 @@
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                Console.Clear();
+                try
+                {
+                
             uint MaxRuns = 20;
             int runs = 0;
-            int[,] seed= null;
+            int[,] seed = null;
 
             Console.WriteLine("App Title: Game Of Life");
             Console.WriteLine("Please, select if you would like to see the Blinker life circle (M) or " +
                 "LifeCircle with Random numbers (R). \t");
             Console.Write("Select : M or R:\t ");
-
+                    
                 ConsoleKey consoleKey = Console.ReadKey().Key;
                 switch (consoleKey)
                 {
@@ -25,8 +31,30 @@
                     {0,0,1,0,0},
                     {0,0,0,0,0}
                    };
+                        GameSeed gameSeed = new GameSeed(seed);
+                        Console.ReadLine();
+                        Console.WriteLine("Start Field");
+                        gameSeed.DrawField();
+                        Console.WriteLine();
 
-                    break;
+                        while (gameSeed.AliveCells() > 0 && runs++ < MaxRuns)
+                        {
+                            Console.WriteLine();
+                            gameSeed.NewCellGeneration();
+                            gameSeed.DrawField();
+                            Console.WriteLine();
+
+                            if (gameSeed.AliveCells() == 0)
+                            {
+                                Console.WriteLine("Everyone is died!");
+                                Console.ReadLine();
+                            }
+                            else
+                            {
+                                Console.ReadLine();
+                            }
+                        }
+                        break;
 
                     case ConsoleKey.R:
                         while (true)
@@ -39,6 +67,7 @@
                                 int row = int.Parse(Console.ReadLine());
                                 Console.Write("The number of columns:\t ");
                                 int column = int.Parse(Console.ReadLine());
+
                                 seed = new int[row, column];
                                 Random random = new Random();
 
@@ -49,6 +78,7 @@
                                         seed[y, x] = random.Next(2); // 0- dead cell, 1- alive
                                     }
                                 }
+
                                 for (int y = 0; y < seed.GetLength(0); y++) // The part which input the array elements.
                                 {
                                     for (int x = 0; x < seed.GetLength(1); x++)
@@ -57,43 +87,49 @@
                                     }
                                     Console.WriteLine();
                                 }
+
+                                GameSeed gameSeedRandom = new GameSeed(seed);
+                                Console.WriteLine("Start Field");
+                                gameSeedRandom.DrawField();
+                                Console.WriteLine();
+
+                                while (gameSeedRandom.AliveCells() > 0 && runs++ < MaxRuns)
+                                {
+                                    Console.WriteLine();
+                                    gameSeedRandom.NewCellGeneration();
+                                    gameSeedRandom.DrawField();
+                                    Console.WriteLine();
+
+                                    if (gameSeedRandom.AliveCells() == 0)
+                                    {
+                                        Console.WriteLine("Everyone is died!");
+                                        Console.ReadLine();
+                                    }
+                                    else
+                                    {
+                                        Console.ReadLine();
+                                    }
+                                }
                             }
                             catch (Exception)
-                            {
-                                Console.WriteLine("Input incorrect data! Please, try again.");
-                                Console.ReadLine();
-                                continue;
+                            {                                   
+                                    Console.WriteLine("Input incorrect data! Please, try again.");
+                                    Console.ReadLine();
+                                    continue;
                             }
                         }
                     default:
+                        
+                        Console.WriteLine("\nYou input incorrect data! Please, try again."); 
                         Console.ReadLine();
-                        Console.WriteLine("You input incorrect data! Please, try again.");
                         break;
+                        }
                 }
-
-                GameSeed gameSeed = new GameSeed(seed);
-                Console.ReadLine();
-                Console.WriteLine("\n Start Field");
-                gameSeed.DrawField();
-                Console.WriteLine();
-
-                while (gameSeed.AliveCells() > 0 && runs++ < MaxRuns)
+                catch (Exception)
                 {
-                    Console.WriteLine();
-                    gameSeed.NewCellGeneration();
-                    gameSeed.DrawField();
-                    Console.WriteLine();
-
-                    if (gameSeed.AliveCells() == 0)
-                    {
-                        Console.WriteLine("Everyone is died!");
-                        Console.ReadLine();
-                    }
-                    else
-                    {
-                        Console.ReadLine();
-                    }
+                    Console.WriteLine();                   
                 }
             }
         }
     }
+}
